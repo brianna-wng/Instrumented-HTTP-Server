497bfb9ea24346963d30d5889fd6f6bd0896d251
Brianna Wang <wang.brianna@gmail.com>
2025-05-28 09:23:29 -0700

    fixed minor typos

--- http-servers.go
+++ http-servers.go
@@ -9,7 +9,7 @@ func hello(w http.ResponseWriter, req *http.Request){
 	fmt.Fprintf(w, "hello\n")
 }
 
-func headers(w http.ResponseWritter, req *http.Request){
+func headers(w http.ResponseWriter, req *http.Request){
 	for name, headers := range req.Header{
 		for _, h := range headers{
 			fmt.Fprintf(w, "%v: %v\n", name, h)
@@ -18,8 +18,8 @@ func headers(w http.ResponseWritter, req *http.Request){
 }
 
 func main() {
-	http.HandleFunc("/hello", "hello")
-	http.HandleFunc("/headers", "headers")
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/headers", headers)
 	
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8090", nil)
 }
